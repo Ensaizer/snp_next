8e7aeb67d6075585f4bec91be83b4c9d8e88ab5a
Anton-CSS <9251580262@mail.ru>
2024-01-19 12:38:22 +0300

    fix registration route and form registration

--- src/app/api/registration/route.ts
+++ src/app/api/registration/route.ts
@@ -1,11 +1,14 @@
 import {NextResponse} from "next/server";
 import prisma from "@/app/keke/prismaClient";
+import bcrypt from 'bcrypt'
 export async function POST(request:Request){
     const data = await request.json();
     data.discount = 10;
     data.isApproved = false;
     data.roleId = +data.role;
     delete data.role;
+    const hash = await bcrypt.hash(data.password, 10)
+    data.password = hash;
     const createUser = async () => {
         const user = await prisma.user.create({
               data,
